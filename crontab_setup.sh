728d4b9affa60be8a5300a1716bb4d33b0035f25
Alessandro Bortolussi <alessandro@alborworld.com>
2021-06-05 19:36:49 +0200

    Fixed sed string replacement expression adding quotes around the env variable values and fixed cron setup file

--- crontab_setup.sh
+++ crontab_setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Create script to source all environment variables for cron
-printenv | sed 's/^\(.*\)$/export \1/g' > /root/project_env.sh
+printenv | sed -E 's/(.*)=(.*)/export \1="\2"/g' > /root/project_env.sh
 
 # Build cron setup file in the cron.d directory
-echo "$CRON_SETTINGS root source /root/project_env.sh && python3 /app/zoom-recording-downloader.py >> /var/log/zoom-recording-downloader.log 2>&1" > /etc/cron.d/cron
+echo "$CRON_SETTINGS . /root/project_env.sh && python3 /app/zoom-recording-downloader.py >> /var/log/cron.log 2>&1" > /etc/cron.d/cron
 
 # Give execution rights on the cron job
 chmod 0644 /etc/cron.d/cron
