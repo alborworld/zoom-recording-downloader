d2dfbfebdd1149e383b8df7e43c2d7ef5109e360
Alessandro Bortolussi <alessandro@alborworld.com>
2025-06-12 17:50:49 +0200

    feat: don't delete cloud recordings, as Zoom will do it automatically after some days

--- crontab_setup.sh
+++ crontab_setup.sh
@@ -10,5 +10,5 @@ printenv | sed -E 's/(.*)=(.*)/export \1="\2"/g' > /root/project_env.sh
 echo "0 0 * * * /usr/sbin/logrotate /etc/logrotate.d/zoom-recording-downloader" | crontab -
 
 # Build cron job definition for the root user
-CRON_JOB="$CRON_SETTINGS . /root/project_env.sh && python3 /app/zoom-recording-downloader.py >> ${LOG_DIRECTORY:-/var/log/zoom-recording-downloader}/app.log 2>> ${LOG_DIRECTORY:-/var/log/zoom-recording-downloader}/error.log && cat ${LOG_DIRECTORY:-/var/log/zoom-recording-downloader}/app.log ${LOG_DIRECTORY:-/var/log/zoom-recording-downloader}/error.log >> /proc/1/fd/1 2>> /proc/1/fd/2"
+CRON_JOB="$CRON_SETTINGS . /root/project_env.sh && python3 /app/zoom-recording-downloader.py --no-delete >> ${LOG_DIRECTORY:-/var/log/zoom-recording-downloader}/app.log 2>> ${LOG_DIRECTORY:-/var/log/zoom-recording-downloader}/error.log && cat ${LOG_DIRECTORY:-/var/log/zoom-recording-downloader}/app.log ${LOG_DIRECTORY:-/var/log/zoom-recording-downloader}/error.log >> /proc/1/fd/1 2>> /proc/1/fd/2"
 echo "$CRON_JOB" | crontab -
